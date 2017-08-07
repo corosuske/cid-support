c39df36da326ea1b47a868b2682844334da8d1ee
wouter simons <wouter.simons@gmail.com>
2017-08-07 17:06:31 +0200

    corrected user-message .. 2nd ip is wifi

--- run.sh
+++ run.sh
@@ -28,7 +28,7 @@ then
   then
     echo -n "                     " 
     echo ${ESSID}
-    echo -n "                Cable IP:"
+    echo -n "                WIFI IP:"
     ifconfig wlan0 | grep 'inet addr:'| grep -v '127.0.0.1' | cut -d: -f2 | awk '{ print $1}'
 
   else
